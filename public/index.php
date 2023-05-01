4375aac01191a75a0a736936c2584dd7ecb023d0
Maxim <max9680@mail.ru>
2023-05-01 21:47:33 +0300

    var_dump

--- public/index.php
+++ public/index.php
@@ -55,6 +55,9 @@ $app->get('/urls', function ($request, $response) {
 
     $checks = $this->get('pdo')->query("SELECT * FROM url_checks ORDER BY id DESC")->fetchAll();
 
+    var_dump($checks);
+    die();
+
     $urlsWCheck = array_map(function ($url) use ($checks) {
         $resultArray = $url;
         $check = collect($checks)->firstWhere('url_id', $url['id']);
