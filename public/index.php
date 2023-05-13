929946a6393b712192586e5ad17904c0ca93ecf5
Maxim <max9680@mail.ru>
2023-05-13 15:57:54 +0300

    remove module WriteDataInDB

--- public/index.php
+++ public/index.php
@@ -12,7 +12,22 @@ use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Exception\ConnectException;
 use GuzzleHttp\Exception\ClientException;
 use DiDom\Document;
-use function Analyzer\writeDataInDB;
+
+function writeDataInDB(\PDO $pdo, \DiDom\Document $document, int $id, \Carbon\Carbon $nowTime, int $statusCode)
+{
+    $h1 = optional($document->first('h1'))->text();
+    $title = optional($document->first('title'))->text();
+    $description = optional($document->first('meta[name=description]'))->getAttribute('content');
+
+    $arrVars = [$id, $nowTime, $statusCode, $h1, $title, $description];
+
+    $stm = $pdo->prepare("INSERT INTO
+                        url_checks (url_id, created_at, status_code, h1, title, description)
+                        VALUES (?, ?, ?, ?, ?, ?)");
+    $stm->execute($arrVars);
+
+    return $pdo->lastInsertId();
+}
 
 session_start();
 
